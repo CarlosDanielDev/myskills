8f6b9a495a56d33b917281ff276086b3da20d3f1
Carlos Daniel <killer.dk20@gmail.com>
2021-05-17 16:01:21 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const App = () => {
+  return (
+    <View style={{
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <Text>
+        My Skills
+      </Text>
+    </View>
+  );
+}
+
+export default App;
