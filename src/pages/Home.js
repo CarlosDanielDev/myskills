b23472b204ba35f2329016988024937995225043
Carlos Daniel <killer.dk20@gmail.com>
2021-05-18 01:10:11 -0300

    chore: layout init

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { 
+    View, 
+    Text,
+    StyleSheet, 
+    TextInput, 
+    Platform,
+    TouchableOpacity
+} from 'react-native';
 
 
 export const Home = () => {
@@ -8,6 +15,23 @@ export const Home = () => {
             <Text style={styles.title}>
                 Ignite
             </Text>
+            <TextInput 
+                style={styles.input}
+                placeholder="New Skill"
+                placeholderTextColor="#555"
+            />
+            <TouchableOpacity 
+                style={styles.button}
+                activeOpacity={0.7}
+            >
+                <Text style={styles.buttonText}>
+                    Add
+                </Text>
+            </TouchableOpacity>
+
+            <Text style={[styles.title, { marginTop: 50 }]}>
+                My Skills
+            </Text>
         </View>
     )
 }
@@ -15,11 +39,34 @@ export const Home = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#121015'
+        backgroundColor: '#121015',
+        paddingVertical: 70,
+        paddingHorizontal: 30,
+
     },
     title: {
-        color: '#fff'
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    input: {
+        backgroundColor: '#1f1e25',
+        color: '#fff',
+        fontSize: 18,
+        padding: Platform.OS === 'ios' ? 15 : 10,
+        marginTop: 30,
+        borderRadius: 7
+    },
+    button: {
+        backgroundColor: '#a370f7',
+        padding: 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 20
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 17,
+        fontWeight: 'bold'
     }
 })
